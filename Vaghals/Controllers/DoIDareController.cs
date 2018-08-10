33f71cce781744439dc65b08f088af915592e2ca
Daniel Björkander <Daniel Bjrkander>
2018-08-10 10:44:12 +0200

    Where are we now?

---
+++ Vaghals/Controllers/DoIDareController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Vaghals.Controllers
+{
+    public class DoIDareController : Controller
+    {
+        // GET
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
